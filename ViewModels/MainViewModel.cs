0201bd3950c7256518acf73f8bc7ff857cec3242
LuckyZhangWeiwei <zhangzhang_10@163.com>
2025-04-01 19:58:37 +0800

    03. Avalonia UI Real World (View Model Basics)

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,23 +1,19 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using Avalonia.Svg.Skia;
 using CommunityToolkit.Mvvm.Input;
 
 namespace BatchProcess3.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    [ObservableProperty] [NotifyPropertyChangedFor(nameof(SideMenuImage))]
+    [ObservableProperty] 
+    [NotifyPropertyChangedFor(nameof(SomeWidth))]
     private bool _sideMenuExpanded = false;
 
-    public SvgImage SideMenuImage => new SvgImage
-    {
-        Source = SvgSource.Load(
-            $"avares://{nameof(BatchProcess3)}/Assets/Images/{(SideMenuExpanded ? "logo" : "icon")}.svg")
-    };
+    public int SomeWidth => _sideMenuExpanded ? 220 : 22;
 
     [RelayCommand]
     private void SideMenuResize()
     {
-        SideMenuExpanded =!SideMenuExpanded;
+        SideMenuExpanded = !SideMenuExpanded;
     }
 }
